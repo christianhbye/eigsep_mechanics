42ddc8c6e32cd505a7c4aba75e67d52d93e7a2ea
Christian Hellum Bye <christian.hellum.bye@gmail.com>
2021-09-28 13:59:42 -0700

    add way to compute pointing angle and compare to accuracy

--- mechanics.py
+++ mechanics.py
@@ -11,17 +11,15 @@ class Platform:
         :param center: coordinates of center of platform
         """
         self.angle = 2*np.pi/3
-        self.pointing_accuracy = 5 * np.pi/180  # radians
+        self.pointing_accuracy = 5 # degrees
         self.center = center
         self.vertices = center_dist*np.array([[np.cos(i*self.angle), np.sin(i*self.angle), 0]
                                               for i in range(tethering_points)])
         if shape == 'triangle':  # draw straight lines between tethering points
-            side1 = [self.vertices[0], self.vertices[1]]
-            side2 = [self.vertices[1], self.vertices[2]]
-
-            #self.sides = np.array([side1, side2])
             self.sides = np.array([[self.vertices[i], self.vertices[(i+1) % tethering_points]] for i in range(tethering_points)])
-        #self.sides = np.array([self.vertices[(i+1) % tethering_points]-self.vertices[i] for i in range(tethering_points)])
+
+        self.normal_vector = np.cross(self.vertices[1]-self.vertices[0], self.vertices[2]-self.vertices[0])
+        self.polar_angle = np.arccos(self.normal_vector[-1]/np.linalg.norm(self.normal_vector)) * 180/np.pi
 
     def plot(self, axes=[0, 1]):
         fig = plt.figure()
