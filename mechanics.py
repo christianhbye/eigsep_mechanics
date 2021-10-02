85086fc389f381bf1fa687fa288d3b3a52cc9b1c
Christian Hellum Bye <christian.hellum.bye@gmail.com>
2021-10-02 12:55:03 -0700

    add method to move platform when attached, illustrate with plot

--- mechanics.py
+++ mechanics.py
@@ -27,9 +27,17 @@ class Platform:
 
     def compute_center(self):
         """
-        The center of the platform is a function of the vertices and the shape
+        The center of the platform is a function of the vertices and the shape. For an equilateral triangle, label the
+        vertices by 1, 2, 3. The midpoint on one side is M = P1 + 1/2 (P2-P1). The center (C) is on the line MP3, with
+        CM = (x/2) tan(30) = x/(2 sqrt(3)) with x being the side length of the triangle.
         """
-        
+        if self.shape == 'triangle':
+            x = self.vertices[1] - self.vertices[0]  # side length
+            m = self.vertices[0] + x/2  # midpoint on the side
+            cm = x/(2*np.sqrt(3))  # length from m to center c
+            cm_dir = self.vertices[2] - m  # direction to center from midpoint
+            c = cm_dir/np.linalg.norm(cm_dir) * cm  # make cm_dir a unit vector and multiply by the length
+            return c
 
     def translate_z(self, dz):
         # when we translate the coordinates of the vertices, a couple of things change
@@ -39,13 +47,14 @@ class Platform:
         if self.shape == 'triangle':  # draw straight lines between tethering points and define a center
             self.sides = np.array([[self.vertices[i], self.vertices[(i+1) % self.tethering_points]]
                                    for i in range(self.tethering_points)])
+        c = self.compute_center()
+        self.center = c
 
-
-
-
-    def plot(self, axes=np.array([0, 1])):
-        fig = plt.figure()
-        ax = fig.add_axes([0, 0, 1, 1])
+    def plot(self, axes=np.array([0, 1]), fig=None, ax=None):
+        if not fig:
+            fig = plt.figure()
+        if not ax:
+            ax = fig.add_axes([0, 0, 1, 1])
         ax1, ax2 = axes
         ax.scatter(self.vertices.T[ax1], self.vertices.T[ax2])
         ax.scatter(self.center[ax1], self.center[ax2])
@@ -53,7 +62,9 @@ class Platform:
         ax.plot([self.center[ax1], self.normal_vector[ax1]], [self.center[ax2], self.normal_vector[ax2]])
         if ax2 == 2:
             ax.axvline(self.center[ax1], ls='--', c='k')
-        plt.show()
+        ax.set_xlabel(chr(120+ax1))
+        ax.set_ylabel(chr(120+ax2))
+        return fig, ax
 
 
 class Anchor:
@@ -63,15 +74,25 @@ class Anchor:
     def __init__(self, r=80, h=30, rope_length=90, anchor_points=3):
 
         self.angle = 2 * np.pi/anchor_points
-        self.vertices = np.array([[r*np.cos(i*self.angle), r*np.sin(i*self.angle), h]
+        self.anchor_points = anchor_points
+        self.vertices = np.array([[r*np.cos(i*self.angle+np.pi/2), r*np.sin(i*self.angle+np.pi/2), h]
                                   for i in range(anchor_points)])
-        self.ropes = rope_length * np.ones(self.vertices.shape[0])
+        self.rope_length = rope_length
 
     def attach_platform(self, platform):
-        offsets = self.vertices - platform.vertices
-        dz = - np.sqrt(self.ropes**2 - (offsets[:, 0]**2 + offsets[:, 1]**2))  # assumes fixed x and y coordinates
-        z = self.vertices[:, 2] + dz
+        offsets_xy = self.vertices[:, :2] - platform.vertices[:, :2]  # rope must cover this offset to fix xy-coords
+        rope_z = np.sqrt(self.rope_length**2 - (offsets_xy[:, 0]**2 + offsets_xy[:, 1]**2))  # remaining rope in z-dir
+        dz = self.vertices[:, 2] - rope_z - platform.vertices[:, 2]  # delta between end of rope and platform vertices
+        platform.translate_z(dz)  # update position of platform
 
-    def plot(self, ax, axes=np.array([0, 1])):
+    def plot(self, platform, axes=np.array([0, 1])):
         ax1, ax2 = axes
-        return ax
+        fig, ax = platform.plot(axes)
+        ax.scatter(self.vertices.T[ax1], self.vertices.T[ax2])
+        for i in range(self.anchor_points):
+            ropesx = [platform.vertices[i][ax1], self.vertices[i][ax1]]
+            ropesy = [platform.vertices[i][ax2], self.vertices[i][ax2]]
+            ax.plot(ropesx, ropesy, c='blue')
+        newax = fig.add_axes([1, 0.7, 0.3, 0.3])
+        fig, newax = platform.plot(axes, fig, newax)
+        return fig, [ax, newax]
